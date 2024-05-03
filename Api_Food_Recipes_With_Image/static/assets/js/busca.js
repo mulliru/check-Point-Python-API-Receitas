23a55e08686b4a796625098c4d7ab501ab89acc9
Luna <lu.faustino2000@gmail.com>
2024-05-03 09:12:57 -0300

    Feito o HTML mostrar titulo e imagem. Adicionando o busca.js

---
+++ Api_Food_Recipes_With_Image/static/assets/js/busca.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    var titles = document.querySelectorAll('.recipe h3');
+
+    titles.forEach(function(title){
+        title.addEventListener('click', function(){
+            var details = this.nextElementSibling;
+            details.classList.toggle('hidden');
+        });
+    });
+};
